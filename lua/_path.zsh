5e976d215d30e1381902cdcd7436293dcd841bdc
zhonghua <zhonghua@cd.tuan800.com>
2016-12-26 16:48:29 +0800

    update

---
+++ lua/_path.zsh
@@ -0,0 +1,5 @@
+# luarocks自己, 以及他装的可执行文件都在这个目录
+export PATH="/usr/local/luarocks-2.2.2/bin:$PATH"
+
+# 临时方案
+#alias luarocks='/usr/local/luarocks-2.2.2/bin/luarocks-5.1'
--- system/_path.zsh
+++ system/_path.zsh
@@ -1,6 +1,3 @@
 #export PATH="/usr/local/lua-5.1.5/bin:./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$DOTFILES/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-# 临时方案
-alias luarocks='/usr/local/luarocks-2.2.2/bin/luarocks-5.1'
